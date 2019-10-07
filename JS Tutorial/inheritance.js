3ce0e9fc048e72993718637d68ed54e54dabfbe2
Rukmal Senavirathne <47842567+rukmals@users.noreply.github.com>
2019-10-07 16:46:54 +0530

    Update inheritance.js

--- JS Tutorial/inheritance.js
+++ JS Tutorial/inheritance.js
@@ -48,3 +48,51 @@ Square.prototype.area=function(){
    /* console.log(-1);
     console.log(-1);
 }*/
+
+/*
+class Rectangle {
+    constructor(w, h) {
+        this.w = w;
+        this.h = h;
+    }
+}
+
+
+class Square extends Rectangle{
+    constructor(w){
+        super(w);
+        //super(area);
+        this.area=function(){
+            return w**2;
+        }
+    }
+}
+Rectangle.prototype.area = function(w,h){
+    return this.w*this.h;
+    //return this.w**2;
+}
+/*Rectangle.prototype.area = function(){
+    return this.w**2;
+}
+*/
+
+
+/*
+ *  Write code that adds an 'area' method to the Rectangle class' prototype
+ */
+
+/*
+ * Create a Square class that inherits from Rectangle and implement its class constructor
+ */
+
+
+if (JSON.stringify(Object.getOwnPropertyNames(Square.prototype)) === JSON.stringify([ 'constructor' ])) {
+    const rec = new Rectangle(3, 4);
+    const sqr = new Square(3);
+    
+    console.log(rec.area());
+    console.log(sqr.area());
+} else {
+    console.log(-1);
+    console.log(-1);
+}*/
